5bc171ae72edce474803fa7fde1e8a989218cccf
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-03-13 10:41:01 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+    
+    attr_reader :knowledge
+    def initialize
+        @knowledge = []
+    end
 
+    def learn(k)
+        @knowledge << k 
+    end
 end
