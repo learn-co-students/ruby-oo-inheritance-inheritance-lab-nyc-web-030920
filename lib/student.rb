4415192fa119a9293584195c32dabd4e53de3ed7
github username <github email address>
2020-03-16 11:30:57 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_reader :knowledge #made this a reader because I think the learn method is basically our writer
+
+    def initialize
+        @knowledge = []
+
+    end
+
+    def learn(fact)
+
+        self.knowledge.push(fact)
+
+    end
 
 end
